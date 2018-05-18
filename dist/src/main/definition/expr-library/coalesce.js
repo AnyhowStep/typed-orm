96ec5a10dabe2980f7b95f980c9c7599c3327cd3
AnyhowStep <anyhowstep@hotmail.com>
2018-05-18 10:22:59 -0400

    Added COALESCE

---
+++ dist/src/main/definition/expr-library/coalesce.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const expr_1 = require("../expr");
+const sd = require("schema-decorator");
+const expr_operation_1 = require("../expr-operation");
+const column_references_operation_1 = require("../column-references-operation");
+function coalesce(left, ...rightArr) {
+    const rightQuery = [];
+    let used = column_references_operation_1.copyReferences(expr_operation_1.usedColumns(left));
+    for (let r of rightArr) {
+        rightQuery.push(expr_operation_1.querify(r));
+        used = column_references_operation_1.combineReferences(used, expr_operation_1.usedColumns(r));
+    }
+    return new expr_1.Expr(used, sd.string(), `COALESCE(${expr_operation_1.querify(left)}, ${rightQuery.join(",")})`);
+}
+exports.coalesce = coalesce;
+//# sourceMappingURL=coalesce.js.map
