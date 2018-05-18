c87dbeb57c5aa85a60daef84df7de9c793d65e5b
AnyhowStep <anyhowstep@hotmail.com>
2018-05-18 10:34:48 -0400

    Added coalesce to export

--- dist/src/main/definition/expr-library/index.d.ts
+++ dist/src/main/definition/expr-library/index.d.ts
@@ -1,3 +1,4 @@
+export * from "./coalesce";
 export * from "./count";
 export * from "./rand";
 export * from "./string";
--- dist/src/main/definition/expr-library/index.js
+++ dist/src/main/definition/expr-library/index.js
@@ -3,6 +3,7 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./coalesce"));
 __export(require("./count"));
 __export(require("./rand"));
 __export(require("./string"));
