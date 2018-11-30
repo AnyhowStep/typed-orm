c3cad0cc2fbfe678ca70c59b2f30ad6148eb3b72
AnyhowStep <anyhowstep@hotmail.com>
2018-11-30 03:06:46 -0500

    Added ColumnMap.toNullable() checks

---
+++ test/compile-time/expected-output/column-map/to-nullable.d.ts
@@ -0,0 +1,105 @@
+/// <reference types="node" />
+import * as sd from "schema-decorator";
+import * as o from "../../../../dist/src/main";
+export declare const columnMap: {
+    readonly x: o.Column<{
+        tableAlias: "someTable";
+        name: "x";
+        assertDelegate: sd.AssertDelegate<number> & {
+            __accepts: number;
+            __canAccept: number;
+        };
+    }>;
+    readonly y: o.Column<{
+        tableAlias: "someTable";
+        name: "y";
+        assertDelegate: sd.AssertDelegate<Date> & {
+            __accepts: Date;
+            __canAccept: string | number | Date;
+        };
+    }>;
+    readonly z: o.Column<{
+        tableAlias: "someTable";
+        name: "z";
+        assertDelegate: sd.AssertDelegate<Buffer> & {
+            __accepts: Buffer;
+            __canAccept: Buffer;
+        };
+    }>;
+};
+export declare const nullable: {
+    readonly x: o.Column<{
+        readonly tableAlias: "someTable";
+        readonly name: "x";
+        readonly assertDelegate: sd.AssertDelegate<number | null>;
+    }>;
+    readonly y: o.Column<{
+        readonly tableAlias: "someTable";
+        readonly name: "y";
+        readonly assertDelegate: sd.AssertDelegate<Date | null>;
+    }>;
+    readonly z: o.Column<{
+        readonly tableAlias: "someTable";
+        readonly name: "z";
+        readonly assertDelegate: sd.AssertDelegate<Buffer | null>;
+    }>;
+};
+export declare const emptyColumnMap: {};
+export declare const emptyNullable: {};
+export declare const mixedColumnMap: {
+    readonly ax: o.Column<{
+        tableAlias: "tableA";
+        name: "ax";
+        assertDelegate: sd.AssertDelegate<number> & {
+            __accepts: number;
+            __canAccept: number;
+        };
+    }>;
+    readonly ay: o.Column<{
+        tableAlias: "tableA";
+        name: "ay";
+        assertDelegate: sd.AssertDelegate<string> & {
+            __accepts: string;
+            __canAccept: string;
+        };
+    }>;
+} & {
+    readonly bx: o.Column<{
+        tableAlias: "tableB";
+        name: "bx";
+        assertDelegate: sd.AssertDelegate<boolean> & {
+            __accepts: boolean;
+            __canAccept: boolean;
+        };
+    }>;
+    readonly by: o.Column<{
+        tableAlias: "tableB";
+        name: "by";
+        assertDelegate: sd.AssertDelegate<Buffer> & {
+            __accepts: Buffer;
+            __canAccept: Buffer;
+        };
+    }>;
+};
+export declare const mixedNullable: {
+    readonly ax: o.Column<{
+        readonly tableAlias: "tableA";
+        readonly name: "ax";
+        readonly assertDelegate: sd.AssertDelegate<number | null>;
+    }>;
+    readonly ay: o.Column<{
+        readonly tableAlias: "tableA";
+        readonly name: "ay";
+        readonly assertDelegate: sd.AssertDelegate<string | null>;
+    }>;
+    readonly bx: o.Column<{
+        readonly tableAlias: "tableB";
+        readonly name: "bx";
+        readonly assertDelegate: sd.AssertDelegate<boolean | null>;
+    }>;
+    readonly by: o.Column<{
+        readonly tableAlias: "tableB";
+        readonly name: "by";
+        readonly assertDelegate: sd.AssertDelegate<Buffer | null>;
+    }>;
+};
---
+++ test/run-time/input/column-map/to-nullable.ts
@@ -0,0 +1,132 @@
+import * as sd from "schema-decorator";
+import * as tape from "tape";
+import * as o from "../../../../dist/src/main";
+
+tape(__filename, (t) => {
+    const columnMap = o.ColumnMapUtil.fromAssertMap(
+        "someTable",
+        {
+            x : sd.naturalNumber(),
+            y : sd.date(),
+            z : sd.buffer(),
+        }
+    );
+
+    const nullable = o.ColumnMapUtil.toNullable(columnMap);
+    t.deepEqual(Object.keys(nullable).length, 3);
+
+    t.deepEqual(
+        nullable.x.assertDelegate("", null),
+        null
+    );
+    t.deepEqual(
+        nullable.x.assertDelegate("", 32),
+        32
+    );
+    t.deepEqual(nullable.x.tableAlias, "someTable");
+    t.deepEqual(nullable.x.name, "x");
+    t.deepEqual(
+        nullable.y.assertDelegate("", null),
+        null
+    );
+    t.deepEqual(
+        nullable.y.assertDelegate("", new Date("2000-01-01")),
+        new Date("2000-01-01")
+    );
+    t.deepEqual(nullable.y.tableAlias, "someTable");
+    t.deepEqual(nullable.y.name, "y");
+    t.deepEqual(
+        nullable.z.assertDelegate("", null),
+        null
+    );
+    t.deepEqual(
+        nullable.z.assertDelegate("", Buffer.from("hey")),
+        Buffer.from("hey")
+    );
+    t.deepEqual(nullable.z.tableAlias, "someTable");
+    t.deepEqual(nullable.z.name, "z");
+
+    t.end();
+});
+
+tape(__filename + "-empty", (t) => {
+    const columnMap = o.ColumnMapUtil.fromAssertMap(
+        "someTable",
+        {}
+    );
+
+    const nullable = o.ColumnMapUtil.toNullable(columnMap);
+    t.deepEqual(Object.keys(nullable).length, 0);
+
+    t.end();
+});
+
+tape(__filename + "-mixed", (t) => {
+    const columnMap = o.ColumnMapUtil.intersect(
+        o.ColumnMapUtil.fromAssertMap(
+            "tableA",
+            {
+                ax : sd.naturalNumber(),
+                ay : sd.string(),
+            }
+        ),
+        o.ColumnMapUtil.fromAssertMap(
+            "tableB",
+            {
+                bx : sd.boolean(),
+                by : sd.buffer(),
+            }
+        )
+    );
+
+    const nullable = o.ColumnMapUtil.toNullable(columnMap);
+    t.deepEqual(Object.keys(nullable).length, 4);
+
+    t.deepEqual(
+        nullable.ax.assertDelegate("", null),
+        null
+    );
+    t.deepEqual(
+        nullable.ax.assertDelegate("", 32),
+        32
+    );
+    t.deepEqual(nullable.ax.tableAlias, "tableA");
+    t.deepEqual(nullable.ax.name, "ax");
+    t.deepEqual(
+        nullable.ay.assertDelegate("", null),
+        null
+    );
+    t.deepEqual(
+        nullable.ay.assertDelegate("", "Boo"),
+        "Boo"
+    );
+    t.deepEqual(nullable.ay.tableAlias, "tableA");
+    t.deepEqual(nullable.ay.name, "ay");
+
+    t.deepEqual(
+        nullable.bx.assertDelegate("", null),
+        null
+    );
+    t.deepEqual(
+        nullable.bx.assertDelegate("", true),
+        true
+    );
+    t.deepEqual(
+        nullable.bx.assertDelegate("", false),
+        false
+    );
+    t.deepEqual(nullable.bx.tableAlias, "tableB");
+    t.deepEqual(nullable.bx.name, "bx");
+    t.deepEqual(
+        nullable.by.assertDelegate("", null),
+        null
+    );
+    t.deepEqual(
+        nullable.by.assertDelegate("", Buffer.from("Bah")),
+        Buffer.from("Bah")
+    );
+    t.deepEqual(nullable.by.tableAlias, "tableB");
+    t.deepEqual(nullable.by.name, "by");
+
+    t.end();
+});
