e7320f60224e98c888c6ae40e6d7cf01a18d7cb7
AnyhowStep <anyhowstep@hotmail.com>
2018-12-02 17:54:33 -0500

    Added Query.innerJoin() already joined tests

---
+++ test/compile-time/input/query/inner-join/already-joined.ts
@@ -0,0 +1,31 @@
+import * as sd from "schema-decorator";
+import * as o from "../../../../../dist/src/main";
+
+const table = o.table(
+    "table",
+    {
+        x : sd.naturalNumber(),
+        y : sd.string(),
+        z : sd.boolean(),
+    }
+);
+const joined1 = o.table(
+    "joined1",
+    {
+        a : sd.date(),
+        b : sd.buffer(),
+        c : sd.string()
+    }
+)
+
+export const query = o.from(table)
+    .innerJoin(
+        joined1,
+        c => [c.y],
+        t => [t.c]
+    )
+    .innerJoin(
+        joined1,
+        c => [c.y],
+        t => [t.c]
+    );
