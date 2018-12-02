2fdc18038fa6f486d3c9d4c8d03ba5389457932b
AnyhowStep <anyhowstep@hotmail.com>
2018-12-02 03:31:27 -0500

    Added Table.setHasExplicitDefaultValue() tests

---
+++ test/compile-time/expected-output/table/set-has-explicit-default-value.d.ts
@@ -0,0 +1,42 @@
+import * as sd from "schema-decorator";
+import * as o from "../../../../dist/src/main";
+export declare const table: o.Table<{
+    readonly alias: "table";
+    readonly name: "table";
+    readonly columns: {
+        readonly x: o.Column<{
+            tableAlias: "table";
+            name: "x";
+            assertDelegate: sd.AssertDelegate<number> & {
+                __accepts: number;
+                __canAccept: number;
+            };
+        }>;
+        readonly y: o.Column<{
+            tableAlias: "table";
+            name: "y";
+            assertDelegate: sd.AssertDelegate<string> & {
+                __accepts: string;
+                __canAccept: string;
+            };
+        }>;
+        readonly z: o.Column<{
+            tableAlias: "table";
+            name: "z";
+            assertDelegate: sd.AssertDelegate<boolean> & {
+                __accepts: boolean;
+                __canAccept: boolean;
+            };
+        }>;
+    };
+    readonly autoIncrement: undefined;
+    readonly id: undefined;
+    readonly candidateKeys: [];
+    readonly generated: [];
+    readonly isNullable: never[];
+    readonly hasExplicitDefaultValue: ("x" | "z")[];
+    readonly mutable: ("x" | "y" | "z")[];
+    readonly parents: [];
+    readonly insertAllowed: true;
+    readonly deleteAllowed: true;
+}>;
