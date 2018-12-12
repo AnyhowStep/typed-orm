c8e42c8c7612e20c27f396428693210aa8212be6
AnyhowStep <anyhowstep@hotmail.com>
2018-12-12 08:00:40 -0500

    Added SELECT run-time test

---
+++ test/run-time/input/query/select/column-map-not-in-join.ts
@@ -0,0 +1,29 @@
+import * as sd from "schema-decorator";
+import * as tape from "tape";
+import * as o from "../../../../../dist/src/main";
+
+tape(__filename, (t) => {
+    const table = o.table(
+        "table",
+        {
+            x : sd.naturalNumber(),
+            y : sd.string(),
+            z : sd.boolean(),
+        }
+    );
+    const otherTable = o.table(
+        "otherTable",
+        {
+            x : sd.naturalNumber(),
+            y : sd.string(),
+            z : sd.boolean(),
+        }
+    );
+
+    t.throws(() => {
+        o.from(table)
+            .select((() => [otherTable.columns]) as any);
+    });
+
+    t.end();
+});
