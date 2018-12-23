f54c42d97b52a44017af4bafa0bcffd6c0603948
AnyhowStep <anyhowstep@hotmail.com>
2018-12-23 10:10:55 -0500

    Refactored bigIntAdd()

--- dist/src/main/v3/expr-library/operator/arithmetic/bigint/add.js
+++ dist/src/main/v3/expr-library/operator/arithmetic/bigint/add.js
@@ -5,43 +5,40 @@ const expr_1 = require("../../../../expr");
 const query_tree_1 = require("../../../../query-tree");
 const dataType = require("../../../../data-type");
 //https://dev.mysql.com/doc/refman/8.0/en/arithmetic-functions.html#operator_plus
-class BigIntAddExpr extends expr_1.Expr {
-    constructor(data, queryTree) {
-        super({
-            usedRef: data.usedRef,
-            assertDelegate: dataType.bigint
-        }, queryTree);
+function tryGetAddQueryTree(rawExpr) {
+    if (expr_1.ExprUtil.isExpr(rawExpr)) {
+        if (rawExpr.queryTree instanceof query_tree_1.Parentheses) {
+            const tree = rawExpr.queryTree.getTree();
+            if (tree instanceof Array) {
+                if (tree.length == 0) {
+                    //This shouldn't happen, in general...
+                    return [];
+                }
+                for (let i = 1; i < tree.length; i += 2) {
+                    if (tree[i] !== "+") {
+                        return undefined;
+                    }
+                }
+                return tree;
+            }
+        }
     }
+    return undefined;
 }
 function bigIntAdd(...arr) {
     const usedRef = raw_expr_1.RawExprUtil.intersectUsedRefTuple(...arr);
     const queryTree = [];
     for (let rawExpr of arr) {
-        if (rawExpr instanceof BigIntAddExpr) {
-            if (rawExpr.queryTree instanceof query_tree_1.Parentheses) {
-                const tree = rawExpr.queryTree.getTree();
-                if (tree instanceof Array) {
-                    if (tree.length == 0) {
-                        //This shouldn't happen, in general...
-                        continue;
-                    }
-                    if (queryTree.length > 0) {
-                        queryTree.push("+");
-                    }
-                    queryTree.push(...tree);
-                }
-                else {
-                    if (queryTree.length > 0) {
-                        queryTree.push("+");
-                    }
-                    queryTree.push(tree);
-                }
+        const addQueryTree = tryGetAddQueryTree(rawExpr);
+        if (addQueryTree != undefined) {
+            if (addQueryTree.length == 0) {
+                continue;
             }
             else {
                 if (queryTree.length > 0) {
                     queryTree.push("+");
                 }
-                queryTree.push(raw_expr_1.RawExprUtil.queryTree(rawExpr));
+                queryTree.push(...addQueryTree);
             }
         }
         else {
@@ -53,13 +50,15 @@ function bigIntAdd(...arr) {
     }
     if (queryTree.length == 0) {
         //TODO Is the addition of zero numbers... zero?
-        return new BigIntAddExpr({
+        return new expr_1.Expr({
             usedRef: usedRef,
+            assertDelegate: dataType.bigint,
         }, raw_expr_1.RawExprUtil.queryTree(0));
     }
     else {
-        return new BigIntAddExpr({
+        return new expr_1.Expr({
             usedRef: usedRef,
+            assertDelegate: dataType.bigint,
         }, queryTree);
     }
 }
---
+++ test/run-time/input/expr-library/operator/arithmetic/bigint/add.ts
@@ -0,0 +1,115 @@
+import * as tape from "tape";
+import * as o from "../../../../../../../dist/src/main";
+
+tape(__filename, (t) => {
+    const table = o.table(
+        "table",
+        {
+            x : o.bigint,
+            y : o.bigint,
+            z : o.bigint,
+            a : o.bigint,
+            b : o.bigint,
+            c : o.bigint,
+        }
+    );
+
+    const bigIntAdd1 = o.bigIntAdd(
+        table.columns.x,
+        table.columns.y
+    );
+    t.deepEqual(
+        new o.SqlFormatter().format(
+            o.QueryTreeUtil.toSql(bigIntAdd1.queryTree)
+        ),
+        "(`table`.`x` + `table`.`y`)"
+    );
+
+    const bigIntAdd2 = o.bigIntAdd(
+        table.columns.x,
+        table.columns.y,
+        table.columns.z,
+    );
+    t.deepEqual(
+        new o.SqlFormatter().format(
+            o.QueryTreeUtil.toSql(bigIntAdd2.queryTree)
+        ),
+        "(`table`.`x` + `table`.`y` + `table`.`z`)"
+    );
+
+    const bigIntAdd3 = o.bigIntAdd(
+        bigIntAdd1,
+        table.columns.a
+    );
+    t.deepEqual(
+        new o.SqlFormatter().format(
+            o.QueryTreeUtil.toSql(bigIntAdd3.queryTree)
+        ),
+        "(`table`.`x` + `table`.`y` + `table`.`a`)"
+    );
+
+    const bigIntAdd4 = o.bigIntAdd(
+        bigIntAdd3,
+        table.columns.b
+    );
+    t.deepEqual(
+        new o.SqlFormatter().format(
+            o.QueryTreeUtil.toSql(bigIntAdd4.queryTree)
+        ),
+        "(\n  `table`.`x` + `table`.`y` + `table`.`a` + `table`.`b`\n)"
+    );
+
+    const bigIntAdd5 = o.bigIntAdd(
+        o.bigIntSub(table.columns.x, table.columns.y),
+        table.columns.z
+    );
+    t.deepEqual(
+        new o.SqlFormatter().format(
+            o.QueryTreeUtil.toSql(bigIntAdd5.queryTree)
+        ),
+        "((`table`.`x` - `table`.`y`) + `table`.`z`)"
+    );
+
+    const bigIntAdd6 = o.bigIntAdd(
+        table.columns.x
+    );
+    t.deepEqual(
+        new o.SqlFormatter().format(
+            o.QueryTreeUtil.toSql(bigIntAdd6.queryTree)
+        ),
+        "`table`.`x`"
+    );
+
+    const bigIntAdd7 = o.bigIntAdd(
+    );
+    t.deepEqual(
+        new o.SqlFormatter().format(
+            o.QueryTreeUtil.toSql(bigIntAdd7.queryTree)
+        ),
+        "0"
+    );
+
+    const bigIntAdd8 = o.bigIntAdd(
+        bigIntAdd7,
+        table.columns.x
+    );
+    t.deepEqual(
+        new o.SqlFormatter().format(
+            o.QueryTreeUtil.toSql(bigIntAdd8.queryTree)
+        ),
+        "(0 + `table`.`x`)"
+    );
+
+    const bigIntAdd9 = o.bigIntAdd(
+        0n,
+        table.columns.x
+    );
+    t.deepEqual(
+        new o.SqlFormatter().format(
+            o.QueryTreeUtil.toSql(bigIntAdd9.queryTree)
+        ),
+        "(0 + `table`.`x`)"
+    );
+
+    t.end();
+});
