e8e4ee691aa970e9f33e49d5cf725a23b980d985
AnyhowStep <anyhowstep@hotmail.com>
2018-12-23 19:53:49 -0500

    Added more tests

---
+++ test/run-time/input/query/map/basic.ts
@@ -0,0 +1,64 @@
+import * as sd from "schema-decorator";
+import * as tape from "tape";
+import * as o from "../../../../../dist/src/main";
+
+tape(__filename, async (t) => {
+    const table = o.table(
+        "table",
+        {
+            x : sd.naturalNumber(),
+            y : sd.string(),
+            z : sd.boolean(),
+        }
+    );
+
+    const query = o.from(table)
+        .select(c => [c.x, c.y]);
+
+    const query1 = query
+        .map((row) => {
+            return {
+                hello : "world" as "world",
+                ...row,
+            }
+        });
+    const query2 = query
+        .map((row) => {
+            return Promise.resolve({
+                hello : "world" as "world",
+                ...row,
+            })
+        });
+    const result1 = await query1._mapDelegate(
+        {
+            x : 32,
+            y : "test"
+        },
+        {
+            x : 32,
+            y : "test"
+        }
+    );
+    const result2 = await query2._mapDelegate(
+        {
+            x : 32,
+            y : "test"
+        },
+        {
+            x : 32,
+            y : "test"
+        }
+    );
+    t.deepEqual(result1, {
+        x : 32,
+        y : "test",
+        hello : "world",
+    });
+    t.deepEqual(result2, {
+        x : 32,
+        y : "test",
+        hello : "world",
+    });
+
+    t.end();
+});
---
+++ test/run-time/input/query/map/chain.ts
@@ -0,0 +1,94 @@
+import * as sd from "schema-decorator";
+import * as tape from "tape";
+import * as o from "../../../../../dist/src/main";
+
+tape(__filename, async (t) => {
+    const table = o.table(
+        "table",
+        {
+            x : sd.naturalNumber(),
+            y : sd.string(),
+            z : sd.boolean(),
+        }
+    );
+
+    const query = o.from(table)
+        .select(c => [c.x, c.y]);
+
+    const query1 = query
+        .map((row) => {
+            return {
+                hello : "world" as "world",
+                ...row,
+            }
+        });
+    const query2 = query
+        .map((row) => {
+            return Promise.resolve({
+                hello : "world" as "world",
+                ...row,
+            })
+        });
+    const query3 = query1
+        .map((row) => {
+            return {
+                chain : "hang low" as "hang low",
+                ...row
+            }
+        });
+    const query4 = query1
+        .map((row) => {
+            return Promise.resolve({
+                chain : "hang low" as "hang low",
+                ...row
+            })
+        });
+    const query5 = query2
+        .map((row) => {
+            return {
+                chain : "hang low" as "hang low",
+                ...row
+            }
+        });
+    const query6 = query2
+        .map((row) => {
+            return Promise.resolve({
+                chain : "hang low" as "hang low",
+                ...row
+            })
+        });
+    const row = {
+        x : 32,
+        y : "test",
+    }
+    const result3 = await query3._mapDelegate(row, row).then(result => result);
+    const result4 = await query4._mapDelegate(row, row).then(result => result);
+    const result5 = await query5._mapDelegate(row, row).then(result => result);
+    const result6 = await query6._mapDelegate(row, row).then(result => result);
+    t.deepEqual(result3, {
+        x : 32,
+        y : "test",
+        hello : "world",
+        chain : "hang low",
+    });
+    t.deepEqual(result4, {
+        x : 32,
+        y : "test",
+        hello : "world",
+        chain : "hang low",
+    });
+    t.deepEqual(result5, {
+        x : 32,
+        y : "test",
+        hello : "world",
+        chain : "hang low",
+    });
+    t.deepEqual(result6, {
+        x : 32,
+        y : "test",
+        hello : "world",
+        chain : "hang low",
+    });
+
+    t.end();
+});
