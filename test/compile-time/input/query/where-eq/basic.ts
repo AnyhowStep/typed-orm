1283e59cdf819452e54d2a10b12c5eff83756ec5
AnyhowStep <anyhowstep@hotmail.com>
2018-12-24 14:57:51 -0500

    Added whereEq() tests

---
+++ test/compile-time/input/query/where-eq/basic.ts
@@ -0,0 +1,14 @@
+import * as sd from "schema-decorator";
+import * as o from "../../../../../dist/src/main";
+
+const table = o.table(
+    "table",
+    {
+        x : sd.naturalNumber(),
+        y : sd.nullable(sd.string()),
+        z : sd.boolean(),
+    }
+);
+
+export const query = o.from(table)
+    .whereEq(c => c.x, 45);
