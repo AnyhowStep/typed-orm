232bd4cc8d69f9d66902e6d46e8297e179d2f512
AnyhowStep <anyhowstep@hotmail.com>
2018-12-24 23:38:52 -0500

    Added tests

---
+++ test/compile-time/input/query/use-joins/basic.ts
@@ -0,0 +1,28 @@
+import * as sd from "schema-decorator";
+import * as o from "../../../../../dist/src/main";
+
+const table = o.table(
+    "table",
+    {
+        x : sd.naturalNumber(),
+        y : sd.string(),
+        z : sd.boolean(),
+    }
+);
+const joined1 = o.table(
+    "joined1",
+    {
+        a : sd.date(),
+        b : sd.number(),
+        y : sd.string()
+    }
+);
+
+const j1 = o.innerJoin(
+    table,
+    joined1,
+    t => [t.x],
+    t => [t.b]
+);
+export const query = o.from(table)
+    .useJoins(j1);
