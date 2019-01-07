f915c90b609dee36f8dbcabf41fd66ce4c2a1559
AnyhowStep <anyhowstep@hotmail.com>
2019-01-06 21:10:09 -0500

    Added isLatestSubExpr()

--- dist/src/main/v3/log/util/execution/index.js
+++ dist/src/main/v3/log/util/execution/index.js
@@ -3,6 +3,21 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./exists"));
+__export(require("./fetch-default"));
+__export(require("./fetch-latest-or-default"));
+__export(require("./fetch-latest-or-error"));
 __export(require("./fetch-latest-or-undefined"));
+__export(require("./fetch-latest-value-or-default"));
+__export(require("./fetch-latest-value-or-error"));
+__export(require("./fetch-latest-value-or-undefined"));
+__export(require("./is-latest-sub-expr"));
 __export(require("./latest-query"));
+__export(require("./latest-sub-query"));
+__export(require("./latest-value-expr"));
+__export(require("./latest-value-query"));
+__export(require("./latest-value-sub-expr"));
+__export(require("./latest-value-sub-query"));
+__export(require("./track-or-error"));
+__export(require("./track"));
 //# sourceMappingURL=index.js.map
---
+++ src/main/v3/log/util/execution/is-latest-sub-expr.ts
@@ -0,0 +1,62 @@
+import * as sd from "schema-decorator";
+import {LogNoTrackedDefaults} from "../../log";
+import {QueryUtil} from "../../../query";
+import {ALIASED} from "../../../constants";
+import {ExprUtil, IExpr} from "../../../expr";
+import {ColumnRefUtil} from "../../../column-ref";
+import {ColumnUtil} from "../../../column";
+import * as exprLib from "../../../expr-library";
+import {TableUtil} from "../../../table";
+import {JoinDeclarationUtil} from "../../../join-declaration";
+
+export type IsLatestSubExpr<
+    LogT extends LogNoTrackedDefaults
+> = (
+    ExprUtil.As<
+        IExpr<{
+            usedRef : ColumnRefUtil.FromColumnArray<
+                ColumnUtil.FromColumnMap<LogT["table"]["columns"]>[]
+            >,
+            assertDelegate : sd.AssertDelegate<boolean>,
+            tableAlias : typeof ALIASED,
+        }>,
+        "isLatest"
+    >
+);
+export function isLatestSubExpr<
+    LogT extends LogNoTrackedDefaults
+> (
+    log : LogT
+) : (
+    IsLatestSubExpr<LogT>
+) {
+    const latest = TableUtil.as(log.table, "latest");
+    const latestOrder = QueryUtil.newInstance()
+        .requireParentJoins(...([log.table] as any))
+        .from(latest as any)
+        .select(() => [
+            latest.columns[log.latestOrder[0].name]
+        ])
+        .where(() => {
+            return JoinDeclarationUtil.innerJoinUsing(
+                latest,
+                log.table as any,
+                c => {
+                    return log.entityIdentifier.map(
+                        columnName => c[columnName]
+                    ) as any;
+                }
+            ).eq()
+        })
+        .orderBy(() => [
+            [
+                latest.columns[log.latestOrder[0].name],
+                log.latestOrder[1]
+            ]
+        ])
+        .limit(1)
+    return exprLib.nullSafeEq(
+        log.table.columns[log.latestOrder[0].name],
+        latestOrder
+    ).as("isLatest");
+}
