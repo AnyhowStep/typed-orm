ef24cac39ec297e28a4f67eae7f68b72226fa305
AnyhowStep <anyhowstep@hotmail.com>
2019-01-18 12:53:27 -0500

    Added fetchAutoIncrementOfTable()

---
+++ src/main/v3/information-schema/execution/fetch-auto-increment-of-table.ts
@@ -0,0 +1,21 @@
+import {QueryUtil} from "../../query";
+import {TABLES} from "../tables";
+import * as exprLib from "../../expr-library";
+import {IConnection} from "../../execution";
+
+export function fetchAutoIncrementOfTable (connection : IConnection, tableName : string) : Promise<bigint|null> {
+    return QueryUtil.newInstance()
+        .from(TABLES)
+        .where(c => exprLib.nullSafeEq(
+            c.TABLE_SCHEMA,
+            exprLib.database()
+        ))
+        .whereEq(
+            c => c.TABLE_NAME,
+            tableName
+        )
+        .select(c => [
+            c.AUTO_INCREMENT
+        ])
+        .fetchValue(connection);
+}
